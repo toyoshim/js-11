cbbb7c8deb83a6f89618fab518eb7e5a36e67972
toyoshim@gmail.com <toyoshim@gmail.com@f4830da4-e70c-046c-ddd1-d7d8dad05fff>
2011-06-12 17:35:09 +0000

    Implement index deferred address calculation mode

--- js/CpuPdp11.js
+++ js/CpuPdp11.js
@@ -798,6 +798,11 @@ CpuPdp11.prototype._indexByMode = function (modeAndR) {
         case CpuPdp11._ADDRESSING_INDEX:
             result = (this._fetchWord() + this.registerSet[r]) & 0xffff;
             break;
+        case CpuPdp11._ADDRESSING_INDEX_DEFERRED:
+            result = this._readShort(
+                    (this._fetchWord() + this.registerSet[r]) & 0xffff,
+                    this.currentMode);
+            break;
         default:
             throw new RangeError("Invalid indexing mode: " + mode);
     }
